ebc653c1bc6aabcb0cb6309a7c71dfa2f7e0232d
Gencer <gencer-genc@outlook.com>
2024-02-05 20:39:17 +0300

    Satınalma işlemi onayı eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,6 +4,12 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
         System.out.println("Merhaba 2");
-        System.out.println("Merhaba 3");
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+        }
+
+
     }
 }
